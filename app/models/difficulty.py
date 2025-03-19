b12478f215eb8732d1da94cbba881e3449b6077c
SanjanaNilanka <nilankasanjana803@gmail.com>
2025-03-19 12:24:25 +0530

    update difficulty types

--- app/models/difficulty.py
+++ app/models/difficulty.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 from typing import Tuple
 
 class Difficulty(BaseModel):
-    input_state: Tuple[float, int, int]
+    input_state: Tuple[float, float, int]
     action_taken: int
     adjusted_difficulty: int
